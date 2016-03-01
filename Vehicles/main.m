4325246e0cd145914eeda7a88192707f86bcdafd
neliSon <[nelsonchow91.nc@gmail.com]>
2016-03-01 11:39:32 -0800

    Initial Commit

---
+++ Vehicles/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Vehicles
+//
+//  Created by Nelson Chow on 2016-03-01.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
